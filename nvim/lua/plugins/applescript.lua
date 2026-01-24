e601a921ecbf839afc0d30830ee9d5721cce071c
Johannes Korbmacher <jkorbmacher@gmail.com>
2026-01-24 23:23:26 +0100

    nvim: add applescript plugin config

---
+++ nvim/lua/plugins/applescript.lua
@@ -0,0 +1,6 @@
+-- Example with Lazy.nvim
+return {
+  -- "vim-scripts/applescript.vim",        -- basic syntax
+  "mityu/vim-applescript", -- syntax + utilities
+  -- "sheerun/vim-polyglot",                -- broad language support including AppleScript
+}
